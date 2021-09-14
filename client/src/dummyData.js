96c7f54f691f3ce7fcfcc00158f569a4eac0d27e
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-09-14 19:54:44 +0400

    верстка компонентов Share, Post, Feed

--- client/src/dummyData.js
+++ client/src/dummyData.js
@@ -2,46 +2,75 @@ export const Users = [
   {
     id: 1,
     profilePicture: 'assets/person/1.jpg',
-    username: 'Safak Kocaoglu',
+    username: 'Сергей',
   },
   {
     id: 2,
     profilePicture: 'assets/person/2.jpg',
-    username: 'Janell Shrum',
+    username: 'Елена',
   },
   {
     id: 3,
     profilePicture: 'assets/person/3.jpg',
-    username: 'Alex Durden',
+    username: 'Марина',
   },
   {
     id: 4,
     profilePicture: 'assets/person/4.jpg',
-    username: 'Dora Hawks',
+    username: 'Евгений',
   },
   {
     id: 5,
     profilePicture: 'assets/person/5.jpg',
-    username: 'Thomas Holden',
+    username: 'Алина',
   },
   {
     id: 6,
     profilePicture: 'assets/person/6.jpg',
-    username: 'Shirley Beauchamp',
+    username: 'Михаил',
   },
   {
     id: 7,
     profilePicture: 'assets/person/7.jpg',
-    username: 'Travis Bennett',
+    username: 'Настя',
   },
   {
     id: 8,
     profilePicture: 'assets/person/8.jpg',
-    username: 'Kristen Thomas',
+    username: 'Илья',
   },
   {
     id: 9,
     profilePicture: 'assets/person/9.jpg',
-    username: 'Gary Duty',
+    username: 'Ольга',
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: 'какой-то пост 111111.',
+    photo: 'assets/post/1.jpg',
+    date: '5 mins ago',
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: 'assets/post/2.jpg',
+    date: '15 mins ago',
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: 'какой-то пост 33333333.',
+    photo: 'assets/post/3.jpg',
+    date: '1 hour ago',
+    userId: 3,
+    like: 61,
+    comment: 2,
   },
 ];
