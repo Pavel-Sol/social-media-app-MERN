e715b7599dfb336d0d09b4fb111c42415b13090d
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-09-30 19:19:08 +0400

    компонент Regisrster связал с сервером

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,28 @@
+import axios from 'axios';
+import {useState} from 'react'
+import { useHistory } from 'react-router';
+
+
 import "./register.css";
 
 const Register = () => {
+  const [username, setUsername ] = useState('')
+  const [email, setEmail ] = useState('')
+  const [password, setPassword ] = useState('')
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const newUser = {username, email, password}
+
+    try {
+     const res =  await axios.post("/auth/register", newUser);
+     history.push("/login");
+    } catch (error) {
+      console.log('error => ', error);
+    }
+  }
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,13 +33,24 @@ const Register = () => {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Password Again" className="loginInput" />
-            <button className="loginButton">Зарегистрироваться</button>
-          </div>
+          <form className="loginBox" 
+            onSubmit={(e) => handleSubmit(e)}>
+            <input 
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username" className="loginInput" />
+            <input
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email" 
+              className="loginInput" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password" className="loginInput" />
+            <button type='submit' className="loginButton">Зарегистрироваться</button>
+          </form>
         </div>
       </div>
     </div>
